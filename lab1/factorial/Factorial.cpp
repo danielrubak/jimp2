1b15defa205b217795165dd729ea1ff43143a46e
DR <danielrubak@gmail.com>
2018-02-27 09:11:51 +0100

    First commit, factorial function

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,13 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+  if (value > 12) {
+    return 0;
+  }
+  if (value < 0) {
+    return value * factorial(value+1);
+  } else if (value == 1 || value == 0) {
+    return 1;
+  }
+  return value * factorial(value-1);
 }
